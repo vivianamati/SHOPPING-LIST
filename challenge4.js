cdd1f4bfc48e025a2d506d823faf6b0fbe578c72
vivianamati <adeliahamati51@gmail.com>
2024-04-12 11:55:48 +0300

    SHOPPING LIST

---
+++ challenge4.js
@@ -0,0 +1,31 @@
+let shoppinglist = [
+    {
+        'item':'Cereal',
+        'quantity':'five boxes',
+        },
+        {
+            'item':'Milk',
+            'quantity':'twelve packets',
+        },
+        {
+            'item':'soap',
+            'quantity':'three bars',
+        },
+        {
+            'item':'cheese',
+            'quantity':'one packet',
+        },
+        {
+            'item':'bread',
+            'quantity':'two loaves',
+        }
+    ]
+
+
+        for(let { item, quantity } of shoppinglist) {
+    console.log (`Juma will shop for ${quantity}  of ${item}`);
+        
+}
+// console.log(`${name} lives in ${address}`);
+        
+     
